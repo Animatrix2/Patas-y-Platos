3233589f1df121fb1035f5cf5d74970896e1097c
Gonzalo Casañas <gonzalofabiansendra@gmail.com>
2024-06-27 16:13:01 -0300

    CSS
    Aqui comenzamos con el css para la pagina en si y como se veria

---
+++ Proyecto/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PET-PENSER</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <div class="">
+        <h2>PET-PENSER</h2>
+            <br>
+            <div class="botones">
+             <a href="iniciar.php">
+                 <button >Iniciar seción</button>
+                 </a>
+            </div>
+
+            <br>
+            <div class="botones">
+             <a href="crear.php">
+                 <button >¿No te has registrado?</button>
+                 </a>
+            </div>
+    </div>
+</body>
+</html>
---
+++ Proyecto/register.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+    // Conexión
+    $conn = mysqli_connect("localhost", "root", "", "usuarios") or die("Conexión fallida: " . mysqli_connect_error());
+
+    // Recuperar los datos del formulario
+    $username = mysqli_real_escape_string($conn, $_POST['usu']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // Verificar si el nombre de usuario ya existe
+    $sql_check = "SELECT * FROM `users` WHERE username = '$username'";
+    $result_check = mysqli_query($conn, $sql_check);
+
+    if (mysqli_num_rows($result_check) > 0) {
+        echo "La cuenta ya existe";
+    } else {
+        // Encriptar la contraseña antes de guardarla 
+        $password_hashed = password_hash($password, PASSWORD_BCRYPT);
+
+        // Insertar el nuevo usuario en la base de datos
+        $sql_add = "INSERT INTO `users` (`username`, `password`) VALUES ('$username', '$password_hashed')";
+        $query = mysqli_query($conn, $sql_add);
+
+        if ($query) {
+            header('Location: bienvenido.php');
+        } else {
+            // Credenciales inválidas, mostrar mensaje de error
+        $mensaje="Nombre de usuario ya usado";
+
+        //---GET--
+        header('Location: crear.php?data='.$mensaje);
+
+           // echo "Error al insertar datos: " . mysqli_error($conn);
+        }
+    }
+
+    // Cerrar la conexión a la base de datos
+    mysqli_close($conn);
+}
+?>
